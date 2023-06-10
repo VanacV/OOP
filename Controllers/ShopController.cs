42464b8a94a13a925dfb6e57ef862d50342a8972
Denis <shden6290@gmail.com>
2023-06-10 12:02:59 +0300

    реализация

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using fireflower_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using fireflower_backend.Models.Interface;
+using fireflower_backend.Storage.Entity;
+
+namespace fireflower_backend.Controllers
+{
+    public class ShopController : Controller
+    {
+       
+        private readonly IShop _shopModel;
+
+        public ShopController(IShop shopModel)
+        {
+            _shopModel = shopModel;
+        }
+
+        [HttpGet("GetAllShop")]
+        public async Task<ActionResult<List<Shop>>> GetAllShop()
+        {
+            try
+            {
+                List<Shop> shops = await _shopModel.GetAllShop();
+                return shops;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed" + ex.Message);
+            }
+        }
+    }
+
+}
+ 
---
+++ Models/Realization/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using fireflower_backend.Models.Interface;
+using fireflower_backend.Storage;
+using fireflower_backend.Storage.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace fireflower_backend.Models.Realization
+{
+    public class ProductModel : IProduct
+    {
+        private readonly MyDbContext _dbContext;
+
+        public ProductModel(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Product>> GetAllProduct()
+        {
+            List<Product> products = await _dbContext.Products.Include(p => p.Payment)
+                .Include(p => p.Shop).Include(p => p.Product_Rating).ToListAsync();
+            return products;
+        }
+    }
+}
