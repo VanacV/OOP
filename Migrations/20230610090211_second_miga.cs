a4bcb9751d99598bbaf246d92b155463c17211f5
VanacV <vanya_ev@mail.ru>
2023-06-10 12:05:19 +0300

    Чето-тут

---
+++ Migrations/20230610090211_second_miga.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fireflower_backend.Migrations
+{
+    public partial class second_miga : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
